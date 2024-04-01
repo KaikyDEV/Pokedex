5b6aa7c0ec46703071489584ae4aa97172be9848
Kaiky Santos <kaikysantosdasilva38@gmail.com>
2024-04-01 13:24:47 -0300

    Integrando API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('Sucesso');
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+// processamento assíncrono
+
+fetch(url).then(function (response) {
+    console.log(response);
+})
+
